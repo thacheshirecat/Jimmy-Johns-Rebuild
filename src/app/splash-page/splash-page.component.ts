619447c0cb4d215ebe1d7e1727a1e81eddea4241
Cheshire Cat <thacheshirecat@gmail.com>
2018-08-17 10:38:46 -0700

    Add content to the footer as well as test to make sure that I understand how to switch views.

--- src/app/splash-page/splash-page.component.ts
+++ src/app/splash-page/splash-page.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-splash-page',
   templateUrl: './splash-page.component.html',
   styleUrls: ['./splash-page.component.css']
 })
-export class SplashPageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class SplashPageComponent
+{
+  @Input() childPageSelection: string;
 }
